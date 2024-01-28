f7ab4161d0ce6b1a87b02ef9f460d500d3fae818
ashok parihar <aparihar@email.arizona.edu>
2024-01-28 13:57:13 -0500

    Developing the code for quiz question(s) and answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,40 @@ function setTime() {
     quiz.setAttribute("style", "display:block");
     switchContent();
 };
+
+// Quiz Array Along With Quiz Questions and Answers
+var quizKey = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choice1: "1. strings",
+        choice2: "2. alerts",
+        answer: "2. alerts",
+    },
+
+    {
+        question: "The condition in an if/else statement is enclosed with ________.",
+        choice1: "1. quotes",
+        choice2: "2. parenthesis",
+        answer: "2. parenthesis",
+    },
+
+];
+
+// Variables for Quiz Questions 
+var questionsEl = document.querySelector(".ask");
+var option1 = document.querySelector(".choice1");
+var option2 = document.querySelector(".choice2");
+var correct = document.querySelector(".correct");
+var wrong = document.querySelector(".wrong");
+
+// Adding The EventListener Attaches An Event Handler to The Element Selected
+// Which Then Will Show The Next Question
+var questionIndex = 0;
+var boxEl = document.querySelector(".quiz-questions");
+boxEl.addEventListener("click", switchContent);
+function switchContent() {
+    questionsEl.textContent = quizKey[questionIndex].question;
+    option1.textContent = quizKey[questionIndex].choice1;
+    option2.textContent = quizKey[questionIndex].choice2;
+};
+
